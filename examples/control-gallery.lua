7aa7d816700d0d55f39927604c2141b9a38a54fd
Matthew Dombroski <matt@mattski.net.nz>
2016-05-27 12:32:28 +1200

    update widget gallery with new styles and examples.
    gallery also functions as a bit of a test area

--- examples/control-gallery.lua
+++ examples/control-gallery.lua
@@ -1,53 +1,56 @@
 local ui = require "libui.core"
 
 local menu = ui.NewMenu( "File" )
-local openitem = menu:AppendItem( "Open" )
-local saveitem = menu:AppendItem( "Save" )
-local item = menu:AppendQuitItem()
+menu:AppendItem( "Open" )
+        :OnClicked( function( m, w )
+            local filename = ui.OpenFile( w )
+            w:SetTitle( filename or "" )
+            if not filename then
+                ui.MsgBoxError( w, "No file selected", "Don't be alarmed!" )
+            else
+                ui.MsgBox( w, "File selected", filename )
+            end
+        end )
+menu:AppendItem( "Save" )
+        :OnClicked( function( m, w )
+            local filename = ui.SaveFile( w )
+            if not filename then
+                ui.MsgBoxError( w, "No file selected", "Don't be alarmed!" )
+            else
+                ui.MsgBox( w, "File selected (don't worry, it's still there)", filename )
+            end
+        end )
+menu:AppendQuitItem()
+
 
 local menu = ui.NewMenu( "Edit" )
-local item = menu:AppendCheckItem( "Checkable Item" )
+menu:AppendCheckItem( "Checkable Item" )
 menu:AppendSeparator()
-local item = menu:AppendItem( "Disabled Item" ):Disable()
-local item = menu:AppendPreferencesItem()
+menu:AppendItem( "Disabled Item" ):Disable()
+menu:AppendPreferencesItem()
 
 local menu = ui.NewMenu( "Help" )
-local item = menu:AppendItem( "Help" )
-local item = menu:AppendAboutItem()
+menu:AppendItem( "Help" )
+menu:AppendAboutItem()
+        :OnClicked( function()
+            local w = ui.NewWindow( "About", 320, 120 ):SetMargined( true )
+            w:SetChild(
+                ui.NewVBox()
+                    :Append( ui.NewHBox():Append( ui.NewLabel( "LibUI Widget Gallery (Lua)\r\n\r\nAuthor: M. Dombroski" ), true ), true )
+                    :Append( ui.NewHBox()
+                        :Append( ui.NewHBox(), true )
+                        :Append( ui.NewButton( "Close" ):OnClicked( function() w:Destroy() end ), true )
+                        :Append( ui.NewHBox(), true ) )
+            ):Show()
+        end )
 
 
-
-local window = ui.NewWindow( "libui Control Gallery", 640, 480, 1 )
-
-
-openitem:OnClicked( function()
-    local filename = ui.OpenFile( window )
-	if not filename == nil then
-		ui.MsgBoxError( window, "No file selected", "Don't be alarmed!" )
-	else
-		ui.MsgBox( window, "File selected", filename )
-	end
-end )
-saveitem:OnClicked( function()
-	local filename = ui.SaveFile( window )
-	if not filename == nil then
-		ui.MsgBoxError( window, "No file selected", "Don't be alarmed!" )
-	else
-		ui.MsgBox( window, "File selected (don't worry, it's still there)", filename )
-	end
-end )
-
-
-ui.ShouldQuit( function()
-	window:Destroy()
-end )
-
-
-window:SetMargined( true )
-window:OnClosing( function()
-	window:Destroy()
-	ui.Quit()
-end )
+local window = ui.NewWindow( "libui Control Gallery", 640, 480, true )
+    :SetMargined( true )
+    :OnClosing( function()
+        window:Destroy()
+        ui.Quit()
+    end )
 
 local box = ui.NewVBox():SetPadded( true )
 window:SetChild( box )
@@ -70,8 +73,9 @@ group:SetChild( inner )
         :Append( ui.NewTimePicker() )
         :Append( ui.NewDateTimePicker() )
         :Append( ui.NewFontButton() )
-        :Append( ui.NewColorButton() )
-        :Append( ui.NewButton( "Button 2" ):OnClicked( function() ui.MsgBox( window, "Button 2", "clicked" ) end ) )
+        :Append( ui.NewColorButton():OnChanged( function( s, r, g, b, a ) print( "new colour " .. r .. "," .. g .. "," .. b .. "," .. a ) end ) )
+        :Append( ui.NewMultilineEntry():SetText( "Hello\r\nWorld" ), true )
+        :Append( ui.NewButton( "Button 2" ):OnClicked( function( b ) ui.MsgBox( window, "Button 2", b:Text() ) b:SetText( "Click!" ) b:Disable() end ) )
 
 local inner2 = ui.NewVBox()
 inner2:SetPadded( true )
@@ -89,19 +93,20 @@ local spinbox = ui.NewSpinbox( 0, 100 )
 inner:Append( spinbox )
 
 local slider = ui.NewSlider( 0, 100 )
+
 inner:Append( slider )
 
 local progress = ui.NewProgressBar()
 inner:Append( progress )
 
-spinbox:OnChanged( function()
-	slider:SetValue( spinbox:Value() )
-	progress:SetValue( spinbox:Value() )
+spinbox:OnChanged( function( sender, value )
+	slider:SetValue( value )
+	progress:SetValue( value )
 end )
 
-slider:OnChanged( function()
-	spinbox:SetValue( slider:Value() )
-	progress:SetValue( slider:Value() )
+slider:OnChanged( function( sender, value )
+	spinbox:SetValue( value )
+	progress:SetValue( value )
 end )
 
 local group = ui.NewGroup( "Lists" )
@@ -112,16 +117,34 @@ local inner = ui.NewVBox()
 inner:SetPadded( true )
 group:SetChild( inner )
 
-inner:Append( ui.NewCombobox():Append( "Combobox Item 1" ):Append( "Combobox Item 2" ):Append( "Combobox Item 3" ) )
-inner:Append( ui.NewCombobox( true ):Append( "Editable Item 1" ):Append( "Editable Item 2" ):Append( "Editable Item 3" ) )
-inner:Append( ui.NewRadioButtons():Append( "Radio Button 1" ):Append( "Radio Button 2" ):Append( "Radio Button 3" ) )
+inner:Append( ui.NewCombobox()
+             :Append( "Combobox Item 1" )
+             :Append( "Combobox Item 2" )
+             :Append( "Combobox Item 3" )
+             :SetSelected( 0 )
+             :OnSelected( function( sender, index ) ui.MsgBox( window, "Selected", tostring(index) ) end )
+            )
+inner:Append( ui.NewCombobox( true )
+             :Append( "Editable Item 1" )
+             :Append( "Editable Item 2" )
+             :Append( "Editable Item 3" )
+             :SetSelected( 0 ) )
+inner:Append( ui.NewRadioButtons()
+             :Append( "Radio Button 1" )
+             :Append( "Radio Button 2" )
+             :Append( "Radio Button 3" ) )
 
 local tab = ui.NewTab()
-tab:Append( "Page 1", ui.NewHBox() )
+tab:Append( "Page 1", ui.NewHBox():Append( ui.NewMultilineEntry():SetText( "Multiline\r\nText" ), true ) )
 tab:Append( "Page 2", ui.NewHBox() )
 tab:Append( "Page 3", ui.NewHBox() )
 inner2:Append( tab, true )
 
+
+ui.ShouldQuit( function()
+	window:Destroy()
+    ui.Quit()
+end )
 
 window:Show()
 ui.Main()
